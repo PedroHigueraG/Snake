0ae08fdf5d64ac0fc9fd40992722973aca6f099a
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-05-07 21:41:24 -0500

    Se agrega el objeto comida, la colisión y su reaparición aleatoria

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import turtle                           #La libreria turtle ayuda a construir la interfaz gráfica
 import time                             #Para obtenier el tiempo
+import random                           #Para usar numeros random
 
 posponer = 0.1
 
 #Configuración
-
 window = turtle.Screen()                #Crea una ventana nueva
 window.title('Snake')                   #Ponemos titulo
 window.bgcolor('#353535')               #Color de fondo
@@ -12,14 +12,21 @@ window.setup(width=600,height=600)      #Redimensionar pantalla
 window.tracer(0)                        #Ayuda a hacer la animación mas placentera
 
 #Cabeza de la serpiente
-
 cabeza = turtle.Turtle()                #Crea un objeto para mostrar en pantalla
 cabeza.speed(0)                         #Se muestra al inciar
-cabeza.shape('square')                  #Se le asigna forma de cuadrado
+cabeza.shape('square')                  #Se le asigna forma de cuadrado, por defecto es 20x20 pixeles
 cabeza.color('#75C46D')                 #Color a la cabeza
 cabeza.penup()                          #Elimina el rastro del objeto
 cabeza.goto(0,0)                        #Centra el objeto
 cabeza.direction = 'stop'               #Asigna direccion, en este caso estatico
+
+#Comida
+comida = turtle.Turtle()
+comida.speed(0)                         
+comida.shape('square')
+comida.color('#D12D2D')
+comida.penup()                          
+comida.goto(0,100)
 
 #Funciones
 
@@ -61,5 +68,11 @@ window.onkeypress(derecha,'Right')
 #Ciclo permanente
 while True:
     window.update()                     #Actualizar la pantalla
+
+    if cabeza.distance(comida)<20:      #Se mira la distancia entre la cabeza y la comida
+        x = random.randint(-280,280)
+        y = random.randint(-280, 280)
+        comida.goto(x,y)                #Se actualiza la posición de la comida a un random
+
     movimiento()                        #Hace el respectivo movimiento
     time.sleep(posponer)                #Hace que se posponga por el tiempo establecido
